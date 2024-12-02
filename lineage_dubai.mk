c486cc8fe27df5b6157d9539e9bbc8d87488195f
Rakhshan Ali <alirakhshan7070@gmail.com>
2024-12-02 15:42:28 +0530

    dubai: risingOS BringUp

--- lineage_dubai.mk
+++ lineage_dubai.mk
@@ -11,6 +11,30 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from dubai device
 $(call inherit-product, device/motorola/dubai/device.mk)
 
+## risingOS-Flags
+
+# Ship Basic Call Recorder
+TARGET_PREBUILT_BCR := true
+
+# Addons
+TARGET_HAS_UDFPS := true
+
+# Gms 
+WITH_GMS := true
+
+TARGET_CORE_GMS := true
+
+TARGET_CORE_GMS_EXTRAS := true
+
+# Ship Pixel Launcher
+TARGET_DEFAULT_PIXEL_LAUNCHER := true
+
+# Lawnchair Prebuilt
+TARGET_PREBUILT_LAWNCHAIR_LAUNCHER := true
+
+# disable/enable blur support, default is false
+TARGET_ENABLE_BLUR := true
+
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
@@ -25,4 +49,7 @@ PRODUCT_GMS_CLIENTID_BASE := android-motorola
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="dubai_g-user 14 U1RD34.80-40 f0ea3-81bef release-keys" \
     BuildFingerprint=motorola/dubai_g/dubai:14/U1RD34.80-40/f0ea3-81bef:user/release-keys \
-    DeviceProduct=dubai_g
+    DeviceProduct=dubai_g \
+    RisingMaintainer="Rakhshan" \
+    RisingChipset="SD 778G+"
+    
